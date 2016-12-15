c68241152bd6f3c8bc98dc6e4ed8ae132cede18a
Show Liu <show.liu@linaro.org>
2016-12-15 16:15:39 +0800

    Add Android.mk for android build

---
+++ ta/Android.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH:= $(call my-dir)
+# 442ed209-b8e2-405e-83845cc78c753428
+#local_module := 8aaaf200-2450-11e4-abe20002a5d5c51b.ta
+local_module := 442ed209-b8e2-405e-83845cc78c753428.ta
+include $(BUILD_OPTEE_MK)
