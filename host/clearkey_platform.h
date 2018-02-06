79e39b68b3d627782d55e673896f0604b18ec30c
Peter Griffin <peter.griffin@linaro.org>
2018-02-06 23:08:13 +0000

    host/clearkey_platform: Add hook for obtaining fd
    Signed-off-by: Peter Griffin <peter.griffin@linaro.org>

---
+++ host/clearkey_platform.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * liABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef OPTEE_CLEARKEY_PLATFORM_H
+#define OPTEE_CLEARKEY_PLATFORM_H
+
+#include <stdint.h>
+/* This file is created for platform specified API prototype */
+
+/* Retrieve memory file descripitor with given memory handle */
+int clearkey_plat_get_mem_fd(void *mem_handle);
+#ifdef SDP_PROTOTYPE
+int allocate_ion_buffer(size_t size, int heap_id);
+int ion_map_and_memcpy(unsigned char* out_data, uint32_t length, int secure_fd);
+#endif
+#endif
